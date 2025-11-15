c92b866cf058519bd73dc661e2b69aa875fb2332
Emanuele-Sgroi <nunzioemanuelesgroi@gmail.com>
2025-11-15 14:41:24 +0000

    Defined sensors functions

---
+++ src/sensors.h
@@ -0,0 +1,16 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+// Initialise all sensors
+void initSensors();
+
+// Read temperature and humidity from DHT22
+void readTempHumidity(float &temperature, float &humidity);
+
+// Check PIR detection
+bool isMotionDetected();
+
+// Check if Bluetooth tag is detected
+bool isTagDetected();
+
+#endif
